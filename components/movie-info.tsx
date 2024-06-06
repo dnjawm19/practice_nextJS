cd2390a95fe83a7dd403c9589ee32661bd09e64d
dnjawm19 <dnjawm19@naver.com>
2024-06-06 19:19:58 +0900

    Dynamic Metadata

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -2,7 +2,7 @@ import { API_URL } from "../app/(home)/page";
 
 import styles from "../styles/movie-info.module.css";
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   console.log(`Fetching Movie Time: ${Date.now()}`);
   //await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(`${API_URL}/${id}`);
